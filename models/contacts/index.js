89d61863ca13ca8c011bd617040ae5e7d0b14543
Elena Berezina <farfalina1908@gmail.com>
2023-12-13 17:09:01 +0200

    made get request

--- models/contacts/index.js
+++ models/contacts/index.js
@@ -1,14 +1,57 @@
-// import fs from 'fs/promises'
+import fs from "fs/promises"
+import path from "path"
+import { nanoid } from "nanoid"
 
-const listContacts = async () => {}
+const contactsPath = path.resolve("models/contacts", "contacts.json")
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+   const data = await fs.readFile(contactsPath)
+   const contacts = JSON.parse(data)
+   return contacts
+}
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+   const contacts = await listContacts()
+   const result = contacts.find((item) => contactId === item.id)
+   if (!result) {
+      return null
+   }
+   return { result }
+}
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+   const contacts = await listContacts()
+   const idx = contacts.findIndex((item) => contactId === item.id)
 
-const updateContact = async (contactId, body) => {}
+   if (idx === -1) {
+      return null
+   }
+   const newContacts = contacts.filter((_, index) => index !== idx)
+
+   await fs.writeFile(contactsPath, JSON.stringify(newContacts))
+
+   return contacts[idx]
+}
+
+const addContact = async (name, email, phone) => {
+   const contacts = await listContacts()
+   const newContact = { id: nanoid(), name, email, phone }
+   contacts.push(newContact)
+   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
+
+   return newContact
+}
+
+const updateContact = async (contactId, body) => {
+   const contacts = await listContacts()
+   const idx = contacts.findIndex((contact) => contact.id === contactId)
+   if (idx === -1) {
+      return null
+   }
+   contacts[idx] = { ...contacts[idx], ...body }
+   await fs.writeFile(contactsPath, JSON.stringify(contacts))
+   return contacts[idx]
+}
 
 export default {
    listContacts,
--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -1,13 +1,26 @@
 import express from "express"
-
+import Joi from "joi"
 import contactsService from "../../models/contacts/index.js"
+
+const joiSchema = Joi.object({
+   name: Joi.string().required(),
+   email: Joi.string().required(),
+   phone: Joi.string().required(),
+})
 
 const contactsRouter = express.Router()
 
 contactsRouter.get("/", async (req, res, next) => {
-   const result = await contactsService.listContacts()
-
-   res.json(result)
+   try {
+      const result = await contactsService.listContacts()
+      res.json({
+         status: "success",
+         code: 200,
+         data: { result },
+      })
+   } catch (error) {
+      next(error)
+   }
 })
 
 contactsRouter.get("/:contactId", async (req, res, next) => {
