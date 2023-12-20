1df07204d043d521689695d687c1c6be2cf846f7
Elena Berezina <farfalina1908@gmail.com>
2023-12-20 18:16:06 +0200

    update contacts.json

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
+import mongoose from "mongoose"
 import app from "./app.js"
 
-app.listen(3000, () => {
+// GWNQo3zH74tipkTP
+const DB_HOST = "mongodb+srv://Olena:GWNQo3zH74tipkTP@cluster0.ml9cauf.mongodb.net/db-contacts?retryWrites=true&w=majority"
+mongoose.connect(DB_HOST)
+.then(() => {
+   app.listen(3000, () => {
    console.log("Server running. Use our API on port: 3000")
 })
+})
+.catch(error => {
+   console.log(error.message)
+   process.exit(1)
+})
+
+
