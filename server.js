527b3eda11d34b2e69636a5f2c027defa1c18cde
Elena Berezina <farfalina1908@gmail.com>
2023-12-21 14:13:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 import mongoose from "mongoose"
 import app from "./app.js"
-import {DB_HOST} from "./config.js"
+
+// import {DB_HOST} from "./config.js"
+
 // GWNQo3zH74tipkTP
 // const DB_HOST = "mongodb+srv://Olena:GWNQo3zH74tipkTP@cluster0.ml9cauf.mongodb.net/db-contacts?retryWrites=true&w=majority"
+
+const {DB_HOST, PORT = 3000} = process.env
+
 mongoose.connect(DB_HOST)
 .then(() => {
-   app.listen(3000, () => {
+   app.listen(PORT, () => {
    // console.log("Server running. Use our API on port: 3000")
-   console.log("Database connection successful")
+   console.log(`Database connection successful on PORT: ${PORT}`)
 })
 })
 .catch(error => {
